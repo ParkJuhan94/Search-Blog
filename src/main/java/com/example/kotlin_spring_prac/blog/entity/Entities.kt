79bda78b08972b8593e6960f9f6c6d24185d3804
ParkJuHan94 <1004sunye@naver.com>
2025-01-15 06:01:09 +0900

    [feat]: Entity 생성

---
+++ src/main/java/com/example/kotlin_spring_prac/blog/entity/Entities.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin_spring_prac.blog.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
+class WordCount(
+    @Id val word: String,
+    val cnt: Int = 0
+)
