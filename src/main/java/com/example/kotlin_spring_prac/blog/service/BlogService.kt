3a1ec0bf87d17699cea2b35e07f4aeb85cb745ec
ParkJuHan94 <1004sunye@naver.com>
2025-01-16 06:25:50 +0900

    [refactor]: BlogService.searchKakao 에서 blogDto의 예외 검증 구현

--- src/main/java/com/example/kotlin_spring_prac/blog/service/BlogService.kt
+++ src/main/java/com/example/kotlin_spring_prac/blog/service/BlogService.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin_spring_prac.blog.service
 
 import com.example.kotlin_spring_prac.blog.dto.BlogDto
+import com.example.kotlin_spring_prac.core.exception.InvalidInputException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -14,6 +15,26 @@ class BlogService {
     lateinit var restApiKey: String
 
     fun searchKakao(blogDto: BlogDto): String? {
+        val msgList = mutableListOf<ExceptionMsg>()
+
+        if (blogDto.query.trim().isEmpty()) {
+            msgList.add(ExceptionMsg.EMPTY_QUERY)
+        }
+
+        if (blogDto.sort.trim() !in arrayOf("accuracy", "recency")) {
+            msgList.add(ExceptionMsg.NOT_IN_SORT)
+        }
+
+        when {
+            blogDto.page < 1 -> msgList.add(ExceptionMsg.LESS_THAN_MIN_PAGE)
+            blogDto.page > 50 -> msgList.add(ExceptionMsg.MORE_THAN_MAX_PAGE)
+        }
+
+        if (msgList.isNotEmpty()) {
+            val message = msgList.joinToString { it.msg }
+            throw InvalidInputException(message)
+        }
+
         val webClient = WebClient
             .builder()
             .baseUrl("https://dapi.kakao.com")
@@ -22,18 +43,25 @@ class BlogService {
 
         val response = webClient
             .get()
-            .uri { it.path("/v2/search/blog")
-                .queryParam("query", blogDto.query)
-                .queryParam("sort", blogDto.sort)
-                .queryParam("page", blogDto.page)
-                .queryParam("size", blogDto.size)
-                .build()}
+            .uri {
+                it.path("/v2/search/blog")
+                    .queryParam("query", blogDto.query)
+                    .queryParam("sort", blogDto.sort)
+                    .queryParam("page", blogDto.page)
+                    .queryParam("size", blogDto.size)
+                    .build()
+            }
             .header("Authorization", "KakaoAK $restApiKey")
             .retrieve()
             .bodyToMono<String>()
 
-        val result = response.block()
-
-        return result
+        return response.block()
     }
+}
+
+private enum class ExceptionMsg(val msg: String) {
+    EMPTY_QUERY("query parameter required"),
+    NOT_IN_SORT("sort parameter one of accuracy and recency"),
+    LESS_THAN_MIN_PAGE("page is less than min"),
+    MORE_THAN_MAX_PAGE("page is more than max")
 }
