40add9bbccd719c2c0871b0a9039dbc5195959d4
ParkJuHan94 <1004sunye@naver.com>
2025-01-18 19:22:25 +0900

    [feat]: Validation을 위한 컨트롤러로 들어오는 파라미터에 @Valid 어노테이션 추가

--- src/main/java/com/example/kotlin_spring_prac/blog/controller/BlogController.kt
+++ src/main/java/com/example/kotlin_spring_prac/blog/controller/BlogController.kt
@@ -2,6 +2,7 @@ package com.example.kotlin_spring_prac.blog.controller;
 
 import com.example.kotlin_spring_prac.blog.dto.BlogDto;
 import com.example.kotlin_spring_prac.blog.service.BlogService;
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ class BlogController(
     val blogService: BlogService
 ) {
     @GetMapping("")
-    fun search(@RequestBody blogDto: BlogDto): String? {
+    fun search(@RequestBody @Valid blogDto: BlogDto): String? {
         return blogService.searchKakao(blogDto)
     }
 }
